3006279ce4d667213e03c7397f6628ec0035066f
6u1d0 <119745878+6u1d0@users.noreply.github.com>
2023-10-26 10:15:47 -0300

    Add files via upload

---
+++ 2510231/MascotaFood-main/index.js
@@ -0,0 +1,10 @@
+    const navToggle = document.querySelector(".nav-toggle");
+    const menu = document.querySelector(".menu");
+
+navToggle.addEventListener('click', () => {
+    menu.classList.toggle("nav-menu_visible");
+});
+
+function alerta(){
+    alert("Gracias por su compra")
+}
